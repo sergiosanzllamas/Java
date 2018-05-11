2fe135b72f0d98bfbcd98b462bb79d719d33b93d
Sergio Sanz Llamas <sanzsergio4ab@gmail.com>
2018-05-11 11:20:49 +0200

    fkjfk

--- Primero/src/Principal.java
+++ Primero/src/Principal.java
@@ -4,7 +4,8 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int numero = 67;
+		int numero = 1;
+		int numero2 = 10;
 		double decimal =  5.8;
 String palabra = "askjas";
 char caracter = '@';
@@ -14,6 +15,14 @@ System.out.println(palabra);
 System.out.println(caracter);
 System.out.println(soyHumano);
 System.out.println(palabra);
+if(numero >0 || numero2<=13) {
+	System.out.println("estoy dentro");
+}else 
+	if(numero == 0) {
+		System.out.println("estoy en else");}
+else {
+		System.out.println("el numero es menor que cero");
+	}
 switch(numero) {
 case 0: System.out.println("SOY CERO");
 break;
@@ -23,16 +32,9 @@ break;
 case 2: System.out.println("SOY dos");
 break;
 default:System.out.println("SOY otro");
-
+break;
 }
-if(numero > 0) {
-	System.out.println("el numero es mayor  que cero");
-}else 
-	if(numero == 0) {
-		System.out.println("el numero es igual  que cero");}
-else {
-		System.out.println("el numero es menor que cero");
-	}
+
 
 	}
 }
---
+++ for/src/principal.java
@@ -0,0 +1,11 @@
+
+public class principal {
+	public static void main(String[] args) {
+		String alumno = "sergio";
+	for(int a = 0; a<=5; a++) {
+	char letra = alumno.charAt(a);
+	System.out.print(letra);
+	}
+	}
+
+}
