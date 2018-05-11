af798080f4f66d3c3e734b1de50ab9497e8084b7
Sergio Sanz Llamas <sanzsergio4ab@gmail.com>
2018-05-11 11:30:59 +0200

    jkdkjd

--- for/src/principal.java
+++ for/src/principal.java
@@ -1,8 +1,8 @@
 
 public class principal {
 	public static void main(String[] args) {
-		String alumno = "s";
-	for(int a = 0; a<alumno.length(); a++) {
+		String alumno = "sanz";
+	for(int a = 3; a<alumno.length(); a--) {
 	char letra = alumno.charAt(a);
     System.out.println(letra);
 	}
